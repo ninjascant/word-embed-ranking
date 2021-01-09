46cb778cccc8c6932d03d9832005a66c1d85622f
Maxim Razhev <cvlt@Maxims-MacBook-Pro.local>
2021-01-09 14:23:14 +0500

    Add Lemmatizer and TextCleaner transformers

---
+++ tests/preprocessing_tests.py
@@ -0,0 +1,52 @@
+import unittest
+from itertools import zip_longest
+from word_embed_ranking.preprocess_corpus import TextCleaner, Lemmatizer
+
+
+class TextCleanerTest(unittest.TestCase):
+    @staticmethod
+    def test_clean_sentences():
+        with open('tests/raw_texts.txt') as file:
+            sentences = file.readlines()[:2]
+        with open('tests/cleaned_texts.txt') as file:
+            correct_results = file.readlines()
+
+        text_cleaner = TextCleaner()
+        cleaned = text_cleaner.transform(sentences, show_progress=False)
+
+        is_correct_res = [res[0].split() == res[1].split()
+                          for res in zip_longest(cleaned, correct_results)]
+        assert all(is_correct_res)
+
+    @staticmethod
+    def test_lemmatize_sentences():
+        with open('tests/raw_texts.txt') as file:
+            sentences = file.readlines()
+
+        text_cleaner = TextCleaner()
+        lemmatizer = Lemmatizer()
+
+        cleaned = text_cleaner.transform(sentences, show_progress=False)
+        preprocessed_texts = lemmatizer.transform(cleaned, show_progress=False)
+
+        is_correct_len = [len(text.original_text.split()) == len(text.lemmatized_text.split())
+                          for text in preprocessed_texts]
+        assert all(is_correct_len)
+
+    def test_remove_pronouns(self):
+        with open('tests/raw_texts.txt') as file:
+            sentences = file.readlines()
+
+        text_cleaner = TextCleaner()
+        cleaned = text_cleaner.transform(sentences, show_progress=False)
+
+        lemmatizer_with_clean = Lemmatizer(remove_pronouns=True)
+        cleaned_preprocessed_texts = lemmatizer_with_clean.transform(cleaned, show_progress=False)
+
+        lemmatizer_without_clean = Lemmatizer(remove_pronouns=False)
+        preprocessed_texts = lemmatizer_without_clean.transform(cleaned, show_progress=False)
+
+        cleaned_has_not_pron = ['-PRON-' not in text.lemmatized_text for text in cleaned_preprocessed_texts]
+        preprocessed_has_pron = ['-PRON-' in text.lemmatized_text for text in preprocessed_texts]
+        assert all(cleaned_has_not_pron)
+        assert any(preprocessed_has_pron)
--- word_embed_ranking/preprocess_corpus.py
+++ word_embed_ranking/preprocess_corpus.py
@@ -1,68 +1,122 @@
 import re
 import string
-import argparse
+from dataclasses import dataclass
 from tqdm.auto import tqdm
-import pandas as pd
 import spacy
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en import English
 from nltk.tokenize import sent_tokenize
 
 tqdm.pandas()
 
-# nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
-
 RE_NUMERIC = re.compile(r"[0-9]+", re.UNICODE)
 RE_PUNCT = re.compile(r'([%s])+' % re.escape(string.punctuation), re.UNICODE)
+RE_WHITESPACE = re.compile(r"(\s)+", re.UNICODE)
 
 
-def clean_text(text):
-    text = text.lower()
-    text = RE_NUMERIC.sub('', text)
-    text = RE_PUNCT.sub('', text)
-    # doc = nlp(text)
-    # tokens = [token for token in doc if not token.is_punct]# [:1_000]
-    return text
+def truncate_to_n_words(text, n_words):
+    return ' '.join(text.split()[:n_words])
 
 
-def lemmatize_text(tokens):
-    return " ".join([token.lemma_ for token in tokens if token.lemma_ != '-PRON-'])
+def _check_nltk():
+    try:
+        sent_tokenize('hello world')
+    except LookupError:
+        import nltk
+        nltk.download('punkt')
 
 
-def postprocess_text(lemmatized_text):
-    spaces_cleaned = filter(lambda x: x != ' ', lemmatized_text.split())
-    return ' '.join(spaces_cleaned)
+class SpacyTokenizer:
+    def __init__(self):
+        nlp = English()
+        self.tokenizer = Tokenizer(nlp.vocab)
+
+    def fit(self):
+        return self
+
+    def transform(self, text):
+        tokens = self.tokenizer(text)
+        return [token.text for token in tokens]
 
 
-def process_text(text):
-    sentences = sent_tokenize(text)
-    cleaned = [clean_text(sentence) for sentence in sentences]
-    cleaned = '. '.join(cleaned)
-    # cleaned = clean_text(text)
-    # lemmatized = lemmatize_text(cleaned)
-    # postprocessed = postprocess_text(lemmatized)
-    return cleaned
+class TextCleaner:
+    @staticmethod
+    def _clean_sentence(text):
+        text = text.lower()
+        text = RE_NUMERIC.sub('', text)
+        text = RE_PUNCT.sub(' ', text)
+        text = RE_WHITESPACE.sub(' ', text)
+        if len(text) > 0 and text[0] == ' ':
+            text = text[1:]
+        if len(text) > 0 and text[-1] == ' ':
+            text = text[:-1]
+        return text
+
+    def _clean_text(self, text):
+        sentences = sent_tokenize(text)
+        cleaned = [self._clean_sentence(sentence) for sentence in sentences]
+        cleaned = '. '.join(cleaned)
+        return cleaned
+
+    def fit(self):
+        return self
+
+    def transform(self, texts, show_progress=True):
+        if show_progress:
+            text_iter = tqdm(texts)
+        else:
+            text_iter = texts
+
+        return [self._clean_text(text) for text in text_iter]
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--corpus-file', type=str)
-    parser.add_argument('--outfile', type=str, default='./corpus_cleaned_train.csv')
-    parser.add_argument('--sample-size', type=int, default=None)
-    args = parser.parse_args()
-
-    if not args.sample_size:
-        texts = pd.read_csv(args.corpus_file)
-    else:
-        texts = pd.read_csv(args.corpus_file, nrows=args.sample_size)
-
-    texts.loc[texts['text'].isna(), 'text'] = ''
-
-    texts['text'] = texts['text'].progress_apply(process_text)
-    texts['query'] = texts['query'].progress_apply(process_text)
-    texts.loc[texts['text'].isna(), 'text'] = ''
-    texts.loc[texts['query'].isna(), 'text'] = ''
-
-    texts.to_csv(args.outfile, index=False)
+@dataclass
+class LemmatizedText:
+    original_text: str
+    lemmatized_text: str
 
 
-if __name__ == '__main__':
-    main()
+class Lemmatizer:
+    def __init__(self, max_len=None, remove_pronouns=False):
+        self.nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+        self.max_len = max_len
+        self.remove_pronouns = remove_pronouns
+
+    def _truncate_texts(self, texts):
+        return [truncate_to_n_words(text, self.max_len) for text in texts]
+
+    def _check_pronouns(self, tokens):
+        if self.remove_pronouns:
+            tokens = [token for token in tokens if token[1] != '-PRON-']
+        return tokens
+
+    def _lemmatize_sentence(self, text):
+        doc = self.nlp(text)
+        tokens = [token for token in doc]
+        tokens = [(token.text, token.lemma_) for token in tokens]
+        tokens = self._check_pronouns(tokens)
+        lemmas = ' '.join([token[1] for token in tokens])
+        raw_text = ' '.join([token[0] for token in tokens])
+        return raw_text, lemmas
+
+    def _lemmatize_text(self, text):
+        sentences = text.split('.')
+        lemmatized_sentences = [self._lemmatize_sentence(sentence) for sentence in sentences]
+        raw_text = '. '.join([item[0] for item in lemmatized_sentences])
+        lemmatized_text = '. '.join([item[1] for item in lemmatized_sentences])
+        return LemmatizedText(raw_text, lemmatized_text)
+
+    def fit(self):
+        return self
+
+    def transform(self, texts, show_progress=True):
+        if self.max_len:
+            texts = self._truncate_texts(texts)
+        if show_progress:
+            text_iter = tqdm(texts)
+        else:
+            text_iter = texts
+
+        texts = [self._lemmatize_text(text) for text in text_iter]
+        print(texts)
+        return texts
