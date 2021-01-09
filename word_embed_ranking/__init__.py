2b325402485f393213f22b70ec4d5f6218c5f45d
Maxim Razhev <cvlt.mao@gmail.com>
2021-01-09 14:25:46 +0500

    Add new transformer to init

--- word_embed_ranking/__init__.py
+++ word_embed_ranking/__init__.py
@@ -1,4 +1,1 @@
-from .ann_index import build_annoy_index
-from .preprocess_corpus import process_text
-from .utils import read_word_vector_file
-from .word_centroid_vectorizer import WordCentroidVectorizer
+from .preprocess_corpus import TextCleaner, Lemmatizer
