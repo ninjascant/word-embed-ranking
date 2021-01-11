9e095142e87059b2b41ee908804eaf35a64734b1
Maxim Razhev <cvlt.mao@gmail.com>
2021-01-11 22:35:36 +0500

    Fix tests

--- tests/preprocessing_tests.py
+++ tests/preprocessing_tests.py
@@ -19,21 +19,6 @@ class TextCleanerTest(unittest.TestCase):
         assert all(is_correct_res)
 
     @staticmethod
-    def test_lemmatize_sentences():
-        with open('tests/raw_texts.txt') as file:
-            sentences = file.readlines()
-
-        text_cleaner = TextCleaner()
-        lemmatizer = Lemmatizer()
-
-        cleaned = text_cleaner.transform(sentences, show_progress=False)
-        preprocessed_texts = lemmatizer.transform(cleaned, show_progress=False)
-
-        is_correct_len = [len(text.original_text.split()) == len(text.preprocessed_text.split())
-                          for text in preprocessed_texts]
-        assert all(is_correct_len)
-
-    @staticmethod
     def test_remove_pronouns():
         with open('tests/raw_texts.txt') as file:
             sentences = file.readlines()
@@ -42,13 +27,13 @@ class TextCleanerTest(unittest.TestCase):
         cleaned = text_cleaner.transform(sentences, show_progress=False)
 
         lemmatizer_with_clean = Lemmatizer(remove_pronouns=True)
-        cleaned_preprocessed_texts = lemmatizer_with_clean.transform(cleaned, show_progress=False)
+        cleaned_preprocessed_texts = lemmatizer_with_clean.transform(cleaned)
 
         lemmatizer_without_clean = Lemmatizer(remove_pronouns=False)
-        preprocessed_texts = lemmatizer_without_clean.transform(cleaned, show_progress=False)
+        preprocessed_texts = lemmatizer_without_clean.transform(cleaned)
 
-        cleaned_has_not_pron = ['-PRON-' not in text.preprocessed_text for text in cleaned_preprocessed_texts]
-        preprocessed_has_pron = ['-PRON-' in text.preprocessed_text for text in preprocessed_texts]
+        cleaned_has_not_pron = ['-PRON-' not in text for text in cleaned_preprocessed_texts]
+        preprocessed_has_pron = ['-PRON-' in text for text in preprocessed_texts]
         assert all(cleaned_has_not_pron)
         assert any(preprocessed_has_pron)
 
